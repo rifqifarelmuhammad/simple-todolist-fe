3df43bff071a734db38bd2e0fc6d63a149d9189c
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-02-01 08:13:28 +0700

    Mini update

--- pages/cloudinary.tsx
+++
@@ -1,9 +0,0 @@
-import { v2 } from 'cloudinary'
-
-export default function Cloudinary(){
-    v2.config({
-        cloud_name:'decwxgqs5',
-        api_key: '183971419469197',
-        api_secret:'p6DanPlkEKN0MWMoQJfwRQzcvfk'
-    })
-}
